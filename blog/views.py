20e3cef54136190f7a8e82ab7688c83c2b88259a
lopezgirls <prueba@apsl.net>
2019-02-14 08:48:34 +0100

    Trabajando en el formulario

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,6 @@ from .models import Post
 
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
-import datetime
-
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -18,12 +15,17 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
-
-
-""" intenta hacer llegar esta vista a urls.py
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html) """
 
